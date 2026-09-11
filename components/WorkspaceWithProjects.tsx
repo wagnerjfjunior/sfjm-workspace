f7934119fa07c082074b9ec4febe092222ad1dfe
Wagner Fernandes <wagnerjfjunior@gmail.com>
2026-09-11 17:11:50 -0300

    feat: load active JCY-P0 workspace state

--- components/WorkspaceWithProjects.tsx
+++ components/WorkspaceWithProjects.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import "@/data/jordanacyrela-project";
+import "@/data/jordanacyrela-p0-state";
 import { WorkspaceHome } from "@/components/WorkspaceHome";
 
 export function WorkspaceWithProjects() {
